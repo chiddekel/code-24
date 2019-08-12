b2b274dcff8d609ccb621ce2df04919d944da884
chiddekel <adam.langowicz@gmail.com>
2019-08-12 19:27:14 +0200

    Add wirtual scroll with cdk scroll module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as faker from 'faker';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'code-24 work in progress.';
+  people;
+
+  title = 'code-24.';
   copyright = '© Adam Łangowicz 2019';
   items = Array.from({length: 100000}).map((_, i) => `Item #${i}`);
+
+  constructor() {
+    this.people = Array(100)
+      .fill(1)
+      .map(_ => {
+        return {
+          name: faker.name.findName(),
+          bio:  faker.hacker.phrase(),
+
+        };
+      });
+  }
 }
